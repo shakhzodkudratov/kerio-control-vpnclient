1759f453c3d51f25fa71270b4a0a9c5118ac4565
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-08-12 18:57:06 +0500

    upd

--- flake.nix
+++ flake.nix
@@ -1,83 +1,94 @@
 {
   description = "Kerio Control VPN Client";
 
-  # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    } @ inputs:
     let
+      lib = nixpkgs.lib;
+      systems = [
+        "x86_64-linux"
+      ];
 
-      # to work with older version of flakes
-      # lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
 
-      # Generate a user-friendly version number.
-      version = "9.4.4-8434";
+      pkgsFor = lib.genAttrs systems (system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        });
 
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-
-    in
-
-    {
-
-      # A Nixpkgs overlay.
-      overlay = final: prev: {
-
-        kerio-control-vpnclient = with final; stdenv.mkDerivation rec {
-          pname = "kerio-control-vpnclient";
-          inherit version;
-
-          src = fetchurl {
-            url = "https://cdn.kerio.com/dwn/control/control-${version}/${pname}-${version}-linux-amd64.deb";
-            # sha256sum kerio-control-vpnclient-9.4.4-8434-linux-amd64.deb
-            # nix hash to-sri --type sha256 ad04c68c2af9928534ea769f2bf2d6aa7784742d24658616c1399407b31783f8
-            hash = "sha256-rQTGjCr5koU06nafK/LWqneEdC0kZYYWwTmUB7MXg/g=";
+      devShellFor = system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
           };
+          script = import ./shell.nix { inherit pkgs; };
+        in
+        script;
+    in
+    {
+      # Nix script formatter
+      formatter =
+        forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
 
-          nativeBuildInputs = [
-            dpkg
-          ];
+      # Development environment
+      devShells = lib.mapAttrs (system: _: devShellFor system) (lib.genAttrs systems { });
 
-          buildInputs = [
-          ];
+      # Output package
+      packages = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage
+          ({ pkgs ? import <nixpkgs> { }, ... }:
+            let
+              lib = pkgs.lib;
+            in
+            pkgs.stdenv.mkDerivation rec {
+              pname = "kerio-control-vpnclient";
+              version = "9.4.4-8434";
 
-          installPhase = ''
-            mkdir -p $out
-            cp -r * $out/
-            mv $out/usr/sbin $out/bin
-          '';
-        };
+              src = pkgs.fetchurl {
+                url = "https://cdn.kerio.com/dwn/control/control-${version}/${pname}-${version}-linux-amd64.deb";
+                # sha256sum kerio-control-vpnclient-9.4.4-8434-linux-amd64.deb
+                # nix hash to-sri --type sha256 ad04c68c2af9928534ea769f2bf2d6aa7784742d24658616c1399407b31783f8
+                hash = "sha256-rQTGjCr5koU06nafK/LWqneEdC0kZYYWwTmUB7MXg/g=";
+              };
 
-      };
+              nativeBuildInputs = with pkgs; [
+                dpkg
+              ];
 
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems
-        (system:
-          {
-            inherit (nixpkgsFor.${system}) kerio-control-vpnclient;
-            default = (nixpkgsFor.${system}).kerio-control-vpnclient;
-          });
+              buildInputs = [
+              ];
 
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.kerio-control-vpnclient);
+              installPhase = ''
+                mkdir -p $out/lib
+                mkdir -p $out/bin
+                mkdir -p $out/etc/systemd/system
 
-      # A NixOS module, if applicable (e.g. if the package provides a system service).
-      nixosModules.kerio-control-vpnclient =
-        { pkgs, ... }:
-        {
-          nixpkgs.overlays = [ self.overlay ];
+                cp -r ./usr/lib/* $out/lib/
+                cp -r ./usr/sbin/* $out/bin/
+                cp -r ./lib/systemd/system/* $out/etc/systemd/system/
+              '';
 
-          environment.systemPackages = [ pkgs.kerio-control-vpnclient ];
-
-          #systemd.services = { ... };
-        };
+              meta = with lib; {
+                homepage = "http://www.kerio.com/control";
+                description = "Kerio Control VPN client for corporate networks.";
+                licencse = licenses.mit;
+                platforms = with platforms; linux;
+              };
+            })
+          { };
+      });
     };
 }
